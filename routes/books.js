49e7cb53d7911e908659b9a5cbccf2697d5e6feb
ruben.verster <ruben.verster@1-grid.com>
2021-10-10 03:05:43 +0200

    book upload functionality

---
+++ routes/books.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const router = express.Router();
+
+const Book = require('../models/book');
+const Author = require('../models/author');
+
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+const uploadPath = path.join('public', Book.coverImageBasePath);
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null, imageMimeTypes.includes(file.mimetype));
+  },
+});
+
+//all books
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.render('books/index', {
+      books: books,
+      searchOptions: req.query,
+    });
+  } catch (error) {
+    res.redirect('/');
+  }
+});
+
+//new book
+router.get('/new', async (req, res) => {
+  renderNewPage(res, new Book());
+});
+
+//create book
+router.post('/', upload.single('cover'), async (req, res) => {
+  const fileName = req.file != null ? req.file.filename : null;
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    coverImageName: fileName,
+    description: req.body.description,
+  });
+
+  try {
+    const newBook = await book.save();
+    // res.redirect(`books/${newBook.id}`)
+    res.redirect(`books`);
+  } catch (error) {
+    if (book.coverImageName != null) {
+      removeBookCover(book.coverImageName);
+    }
+    renderNewPage(res, book, true);
+  }
+});
+
+const removeBookCover = (fileName) => {
+  fs.unlink(path.join(uploadPath, fileName), (err) => {
+    if (err) console.err(err);
+  });
+};
+
+const renderNewPage = async (res, book, hasError = false) => {
+  try {
+    const authors = await Author.find({});
+    const params = {
+      authors: authors,
+      book: book,
+    };
+
+    if (hasError) params.errorMessage = 'Error Creating Book';
+
+    res.render('books/new', params);
+  } catch (error) {
+    res.redirect('/books');
+  }
+};
+
+module.exports = router;
